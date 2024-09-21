acd89237ede894ae0c5fd3579cc8aa30f41802ea
Jisu Kim <jisu15.kim@gmail.com>
2024-09-21 20:19:58 +0900

    [WEAV-48] CTA Button Preview 생성

---
+++ Projects/Features/DesignPreview/Sources/DesignButtonView.swift
@@ -0,0 +1,55 @@
+//
+//  DesignButtonView.swift
+//  DesignPreview
+//
+//  Created by 김지수 on 9/21/24.
+//  Copyright © 2024 com.weave. All rights reserved.
+//
+
+import SwiftUI
+import DesignCore
+
+struct DesignButtonView: View {
+    
+    @FocusState var isTextFieldFocused
+    @State var text = ""
+    
+    var body: some View {
+        VStack(alignment: .center) {
+            Spacer()
+            TextField("Placeholder", text: $text)
+                .focused($isTextFieldFocused)
+            CTAButton(
+                title: "타이틀",
+                backgroundStyle: DesignCore.Colors.blue300
+            ) {}
+            .padding(.horizontal, 36)
+            
+            CTAButton(
+                title: "타이틀",
+                backgroundStyle: DesignCore.Colors.darkGreen
+            ) {}
+            .padding(.horizontal, 36)
+            
+            Spacer()
+            
+            CTABottomButton(
+                title: "다음",
+                backgroundStyle: LinearGradient.gradientA,
+                isActive: true,
+                keyboardShown: isTextFieldFocused
+            ) {
+                if isTextFieldFocused {
+                    isTextFieldFocused.toggle()
+                }
+            }
+        }
+        .ignoresSafeArea(.container, edges: .bottom)
+        .navigationTitle("CTA Button")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    DesignButtonView()
+}
