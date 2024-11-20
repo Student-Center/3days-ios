c73aff288b3f5c3e1d032993516960c187f3d5b9
Jisu Kim <jisu15.kim@gmail.com>
2024-11-21 01:19:50 +0900

    [WEAV-137] compile error resolve '

--- Projects/Features/Home/UnitTest/WidgetUnitTest.swift
+++ Projects/Features/Home/UnitTest/WidgetUnitTest.swift
@@ -22,7 +22,10 @@ struct WidgetUnitTest {
         self.selectionState = WidgetSelectionModel()
         self.selectionIntent = WidgetSelectionIntent(
             model: selectionState,
-            input: .init(successHandler: {})
+            input: .init(
+                successHandler: {
+                }
+            )
         )
         
         self.writeState = WidgetWritingModel()
